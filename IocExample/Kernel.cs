27c93054a458b60f3bd0b245a5ab1d9b4b7887a6
Deadcreep <tolla.von@gmail.com>
2018-10-11 12:36:43 +0500

    complete

---
+++ IocExample/Kernel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocExample
+{
+    class Kernel
+    {
+        Dictionary<Type, Type> dictionaryForTypeToType;
+        Dictionary<Type, object> dictionaryForTypeToObject;
+
+        public Kernel()
+        {
+            dictionaryForTypeToType = new Dictionary<Type, Type>();
+            dictionaryForTypeToObject = new Dictionary<Type, object>();
+        }
+
+        public void BindToType<T>(Type type)
+        {
+            var keyType = typeof(T);
+            dictionaryForTypeToType.Add(keyType, type);
+        }
+
+        public void BindToObject<T>(object obj)
+        {
+            var keyType = typeof(T);
+            dictionaryForTypeToObject.Add(keyType, obj);
+        }
+
+        public T Get<T>()
+        {
+            var keyType = typeof(T);
+            if (dictionaryForTypeToType.ContainsKey(keyType))
+            {
+                var bindType = dictionaryForTypeToType[keyType];
+                var ctorInfo = Utils.GetSingleConstructor(bindType);
+                var parameters = ctorInfo.GetParameters();
+                if (!(parameters is null))
+                {
+                    var parametersObjects = new List<object>();
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        var type = parameter.ParameterType;
+                        parametersObjects.Add(Get(type));
+                    }
+                    return (T)Utils.CreateInstance(bindType, parametersObjects);
+                }
+                return (T)Utils.CreateInstance(bindType);
+            }
+            else if (dictionaryForTypeToObject.ContainsKey(keyType))
+                return (T)dictionaryForTypeToObject[keyType];
+            else throw new ArgumentNullException("This type has not been binded yet.");
+        }
+
+        private object Get(Type keyType)
+        {
+            if (dictionaryForTypeToType.ContainsKey(keyType))
+            {
+                var bindType = dictionaryForTypeToType[keyType];
+                var ctor = Utils.GetSingleConstructor(bindType);
+                var parameters = ctor.GetParameters();
+                if (!(parameters is null))
+                {
+                    var parametersObjects = new List<object>();
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        var type = parameter.ParameterType;
+                        parametersObjects.Add(Get(type));
+                    }
+                    return Utils.CreateInstance(bindType, parametersObjects);
+                }
+                return Utils.CreateInstance(bindType);
+            }
+            else if (dictionaryForTypeToObject.ContainsKey(keyType))
+                return dictionaryForTypeToObject[keyType];
+            else throw new ArgumentNullException("This type has not been binded yet.");
+        }
+
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using IocExample.Classes;
+using Ninject;
+
+namespace IocExample
+{
+    class Program
+    {
+        //Ninject
+        //static void Main(string[] args)
+        //{
+        //    IKernel kernel = new StandardKernel();
+        //    kernel.Bind<ILogger>().To<ConsoleLogger>();
+        //    kernel.Bind<IConnectionFactory>()
+        //        .ToConstructor<SqlConnectionFactory>(k => new SqlConnectionFactory("SQL Connection", k.Inject<ILogger>()))
+        //        .InSingletonScope();
+        //    kernel.Bind<RestClient>().ToConstructor<RestClient>(k => new RestClient("API KEY"));
+        //    kernel.Bind<CacheService>().To<CacheService>();
+        //    kernel.Bind<CommandExecutor>().To<CommandExecutor>();
+        //    kernel.Bind<QueryExecutor>().To<QueryExecutor>();
+        //    kernel.Bind<UserService>().To<UserService>();
+        //    kernel.Bind<CreateUserHandler>().To<CreateUserHandler>();
+        //    var createUserHandler = kernel.Get<CreateUserHandler>();
+        //    createUserHandler.Handle();
+        //}
+
+        static void Main(string[] args)
+        {
+            Kernel kernel = new Kernel();
+
+            kernel.BindToType<ILogger>(typeof(ConsoleLogger));
+            kernel.BindToType<CreateUserHandler>(typeof(CreateUserHandler));
+            kernel.BindToType<CacheService>(typeof(CacheService));
+            kernel.BindToType<CommandExecutor>(typeof(CommandExecutor));
+            kernel.BindToType<QueryExecutor>(typeof(QueryExecutor));
+            kernel.BindToType<UserService>(typeof(UserService));
+            kernel.BindToObject<IConnectionFactory>(new SqlConnectionFactory("SQL Connection", kernel.Get<ILogger>()));
+            kernel.BindToObject<RestClient>(new RestClient("API KEY"));
+
+
+            var createUserHandler = kernel.Get<CreateUserHandler>();
+
+            createUserHandler.Handle();
+
+        }
+
+    }
+}
